6ea92e67a3800aec4b46def7656beab90b2e7123
Brady Kelly <brady@bradykelly.net>
2021-11-07 15:34:42 +0200

    Added basic worker service

---
+++ Volvox.Helios.Worker/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Volvox.Helios.Core.Bot;
+using Volvox.Helios.Core.Bot.Utilities;
+using Volvox.Helios.Core.Modules.Command;
+using Volvox.Helios.Core.Modules.Command.Commands;
+using Volvox.Helios.Core.Modules.Command.Framework;
+using Volvox.Helios.Core.Modules.Common;
+
+namespace Volvox.Helios.Worker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Settings
+                    services.AddSingleton<IDiscordSettings, DiscordSettings>();
+
+                    // Bot
+                    services.AddSingleton<DiscordSocketClient>();
+                    services.AddSingleton<IBot, Bot>();
+
+                    // Hosted services
+                    services.AddHostedService<Worker>();
+
+                    // Commands
+                    services.AddSingleton<IModule, CommandManager>();
+                    services.AddSingleton<ICommand, AboutCommand>();
+
+                    // All Modules
+                    services.AddSingleton<IList<IModule>>(s => s.GetServices<IModule>().ToList());
+                    services.AddSingleton<IList<ICommand>>(s => s.GetServices<ICommand>().ToList());
+                });
+    }
+}
---
+++ Volvox.Helios.Worker/Worker.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Volvox.Helios.Core.Bot;
+
+namespace Volvox.Helios.Worker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IBot _bot;
+
+        public Worker(ILogger<Worker> logger, IBot bot)
+        {
+            _logger = logger;
+            _bot = bot;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Worker is starting bot at: {time}", DateTimeOffset.Now);
+
+            // ReSharper disable once AsyncVoidLambda
+            stoppingToken.Register(async () => await _bot.Stop());
+            await _bot.Start();
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _bot.Stop();
+
+            _logger.LogInformation("Worker has stopped bot at {time}", DateTimeOffset.Now);
+        }
+    }
+}
